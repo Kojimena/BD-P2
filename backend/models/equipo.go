9c57ff3d18d85b9463b13e1f108243aa4dc8ebdd
Mark Albrand <mark.albrand5@gmail.com>
2024-04-13 14:25:45 -0600

    Correcciones

--- backend/models/equipo.go
+++ backend/models/equipo.go
@@ -1,15 +1,11 @@
 package models
 
-import (
-	"time"
-)
-
 type Equipo struct {
-	Nombre               string    `json:"nombre" binding:"required"`
-	Deporte              string    `json:"deporte" binding:"required"`
-	Pais                 string    `json:"pais" binding:"required"`
-	Division             string    `json:"division" binding:"required"`
-	FechaEstablecimiento time.Time `json:"fecha_establecimiento" binding:"required"`
+	Nombre               string `json:"nombre" binding:"required"`
+	Deporte              string `json:"deporte" binding:"required"`
+	Pais                 string `json:"pais" binding:"required"`
+	Division             string `json:"division" binding:"required"`
+	FechaEstablecimiento string `json:"fecha_establecimiento" binding:"required"`
 }
 
 type RelationApoyaEquipo struct {
