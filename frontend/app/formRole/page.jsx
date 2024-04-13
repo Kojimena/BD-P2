f9a4522c465ed80d34c0711b35d663d727ac2785
Jimena Hernández <her21199@uvg.edu.gt>
2024-04-13 00:36:33 -0600

    signup
    Co-Authored-By: Melissa Pérez <84192851+MelissaPerez09@users.noreply.github.com>
    Co-Authored-By: Mark Albrand <mark.albrand@protonmail.com>

---
+++ frontend/app/formRole/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import FormRol from '@/components/FormRol/FormRol'
+
+const FormRole = () => {
+  return (
+    <div className="w-full min-h-screen isolate pt-20 bg-black">
+        <FormRol />
+    </div>
+  )
+}
+
+export default FormRole
--- frontend/components/FormSignUp/FormSignUp.jsx
+++ frontend/components/FormSignUp/FormSignUp.jsx
@@ -1,26 +1,36 @@
 "use client";
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useRouter } from "next/navigation";
 import PopUp from '../PopUp/PopUp';
+import FormRol from '../FormRol/FormRol';
 
 
 const FormSignUp = () => {
   const router = useRouter();
   const [nameInput, setNameInput] = useState('')
-  const [emailInput, setEmailInput] = useState('')
-  const [addressInput, setAddressInput] = useState('')
+  const [apellidoInput, setApellidoInput] = useState('')
+  const [fechaInput, setFechaInput] = useState('')
+  const [usernameInput, setUsernameInput] = useState('')
   const [passwordInput, setPasswordInput] = useState('')
   const [error, setError] = useState('')
   const [showPopUp, setShowPopUp] = useState(false)
   const [gender, setGender] = useState('Género')
   const [isOpen, setIsOpen] = useState(false)
+  const [isOpen2 , setIsOpen2] = useState(false)
+  const [role, setRole] = useState('Rol')
+  const [datasend, setDatasend] = useState({})
+  const [showForm, setShowForm] = useState(false)
 
   const handleNameChange = (e) => {
     setNameInput(e.target.value)
   }
 
-  const handleEmailChange = (e) => {
-    setEmailInput(e.target.value)
+  const handleApellidoChange = (e) => {
+    setApellidoInput(e.target.value)
+  }
+
+  const handleUsername = (e) => {
+    setUsernameInput(e.target.value)
   }
 
   const handleAddressChange = (e) => {
@@ -31,32 +41,29 @@ const FormSignUp = () => {
     setPasswordInput(e.target.value)
   }
 
+  const handleFechaChange = (e) => {
+    setFechaInput(e.target.value)
+  }
+
+  useEffect(() => {
+    if (Object.keys(datasend).length > 0) {
+      setShowForm(true)
+    }
+  }, [datasend])
+
   const handleFormSubmit = async (e) => {
     e.preventDefault()
     const data = {
-      "name": nameInput,
-      "address": addressInput,
-      "email": emailInput,
-      "password": passwordInput
+      "apellido": apellidoInput,
+      "fecha_nacimiento": fechaInput,
+      "genero": gender,
+      "nombre": nameInput,
+      "password": passwordInput,
+      "usuario": usernameInput,
+      "conexiones":[""],
+      "publicaciones":[""],
     }
-    if (!nameInput || !emailInput || !addressInput || !passwordInput) {
-      setShowPopUp(true)
-      setError("Por favor complete todos los campos.")
-      return
-    }
-    const response = await fetch('https://bd2-markalbrand56.koyeb.app/user/signup/', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data)
-    })
-    if (response.ok) {
-      router.push('/login')
-    } else {
-      setShowPopUp(true)
-      setError("No se pudo registrar. Intente nuevamente.")
-    }
+    setDatasend(data)
   }
 
   const handleLogin = () => {
@@ -64,9 +71,9 @@ const FormSignUp = () => {
   }
 
   return (
-    <form className=' p-10 flex justify-center items-center flex-col'>
+    <form className=' p-10 flex justify-start items-center'>
       {showPopUp ? <PopUp error={error} /> : 
-      <><div className="w-1/3">
+      <><div className="w-1/2">
           <div className="flex flex-col">
             <h2 className="font-montserrat text-base font-semibold leading-7 text-white">Personal Information</h2>
             <div className="flex flex-col">
@@ -92,19 +99,7 @@ const FormSignUp = () => {
                     type="text"
                     autoComplete="given-name"
                     className="font-montserrat block w-full rounded-md border-0 p-2 text-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-brown sm:text-sm sm:leading-6 focus-visible:outline-none"
-                  />
-                </div>
-              </div>
-
-              <div className="sm:col-span-3">
-                <label className="label">
-                  Número de teléfono
-                </label>
-                <div className="mt-2">
-                  <input
-                    type="text"
-                    autoComplete="tel"
-                    className="font-montserrat block w-full rounded-md border-0 p-2 text-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-brown sm:text-sm sm:leading-6 focus-visible:outline-none"
+                    onChange={handleApellidoChange}
                   />
                 </div>
               </div>
@@ -117,6 +112,7 @@ const FormSignUp = () => {
                   <input
                     type="date"
                     autoComplete="bday"
+                    onChange={handleFechaChange}
                     className="font-montserrat block w-full rounded-md border-0 p-2 text-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-brown sm:text-sm sm:leading-6 focus-visible:outline-none"
                   />
                 </div>
@@ -136,6 +132,20 @@ const FormSignUp = () => {
               </div>
 
               <div className="sm:col-span-3">
+              <div className="dropdown dropdown-bottom py-2 w-full">
+                  <div tabIndex={0} role="button" className="btn m-1 bg-kaqui hover:bg-brown text-white w-full" onClick={() => setIsOpen2(!isOpen2)}>{role}</div>
+                  {isOpen2 && (
+                    <ul tabIndex={0} className="dropdown-content bg-kaqui z-[1] menu p-2 shadow-md text-white rounded-box w-52">
+                      <li><a onClick={() => {setRole('student'); setIsOpen2(false);}}>Estudiante</a></li>
+                      <li><a onClick={() => {setRole('teacher'); setIsOpen2(false);}}>Maestro</a></li>
+                      <li><a onClick={() => {setRole('both'); setIsOpen2(false);}}>Ambas</a></li>
+                    </ul>
+                  )}
+                </div>
+              </div>
+
+
+              <div className="sm:col-span-3">
                 <label className="label">
                   Nombre de Usuario
                 </label>
@@ -143,6 +153,7 @@ const FormSignUp = () => {
                   <input
                     type="text"
                     autoComplete="username"
+                    onChange={handleUsername}
                     className="font-montserrat block w-full rounded-md border-0 p-2 text-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-brown sm:text-sm sm:leading-6 focus-visible:outline-none"
                   />
                 </div>
@@ -155,13 +166,13 @@ const FormSignUp = () => {
                   <input
                     type="text"
                     autoComplete="street-address"
-                    className="font-montserrat block w-full rounded-md border-0 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-brown sm:text-sm sm:leading-6 focus-visible:outline-none"
+                    className="font-montserrat block w-full rounded-md border-0 p-2 text-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-brown sm:text-sm sm:leading-6 focus-visible:outline-none"
                     onChange={handlePasswordChange} />
                 </div>
               </div>
             </div>
           </div>
-        </div><div className="mt-6 flex items-center justify-between gap-x-6 lg:flex-row flex-col">
+          <div className="mt-6 flex items-center justify-between gap-x-6 lg:flex-row flex-col">
             <button
               type="button"
               onClick={handleLogin}
@@ -169,16 +180,21 @@ const FormSignUp = () => {
               Already have an account? - Login
             </button>
             <div className="flex items-center gap-x-2">
-              <button
-                type="submit"
-                className="font-montserrat rounded-md bg-white px-3 py-2 text-sm font-semibold text-kaqui shadow-sm hover:bg-kaqui hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-none"
-                onClick={handleFormSubmit}
-              >
-                Sign up
-              </button>
+            <button
+              type="submit"
+              onClick={handleFormSubmit}
+              className="font-montserrat rounded-md bg-white px-3 py-2 text-sm font-semibold text-kaqui shadow-sm hover:bg-kaqui hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-none"
+            >
+              Continuar
+            </button>
             </div>
-          </div></>
+          </div>
+        </div>
+        </>
       }
+      <div className="w-1/2 flex justify-center">
+        {showForm && <FormRol lastdata={datasend} role={role} />}
+      </div>
     </form>
   )
 }
